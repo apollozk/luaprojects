17708d8e748a2ad8c74775c9f41e504beadca2be
apollozk <80016879+apollozk@users.noreply.github.com>
2021-03-03 22:38:08 -0400

    Add files via upload

---
+++ apolloG.lua
@@ -0,0 +1,37 @@
+--------- Config marker de coletar peças
+
+configC = { 
+	posX = -2424.05078,  -- X, y e z do marker 
+	posY = -611.16699,
+	posZ = 132.55893,
+	tamanho = 4,  -- tamanho
+	r = 255, -- cores (r - vermelho, g - verde e b - azul)
+	g = 255,
+	b = 0
+}
+
+-------- Config marker de vender peças
+
+configV = {
+	posX2 = 2421.33740,
+	posY2 = -614.33057,
+	posZ2 = 132.56250,
+	tamanhoV = 2,
+	r2 = 255,
+	g2 = 255,
+	b2 = 0
+}
+
+------- config ped
+
+configPed = { 
+	pedX = -2412.84546,
+	pedY = -606.45612,
+	pedZ = 132.56250,
+	skinT = 125
+}
+
+-------- config da função de ficar procurado
+configProcurado = {
+	nivel = 2
+}
